949d319dd9af6c95ad823caf77f48d060de2d165
solarpedia <duttarintu6969@gmail.com>
2026-07-18 03:10:21 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,58 @@
+const slides=document.querySelectorAll(".slide");
+const dots=document.querySelectorAll(".dot");
+const next=document.querySelector(".next");
+const prev=document.querySelector(".prev");
 
+let current=0;
+
+function showSlide(index){
+
+slides.forEach(slide=>slide.classList.remove("active"));
+dots.forEach(dot=>dot.classList.remove("active"));
+
+slides[index].classList.add("active");
+dots[index].classList.add("active");
+
+}
+
+next.onclick=()=>{
+
+current++;
+
+if(current>=slides.length) current=0;
+
+showSlide(current);
+
+}
+
+prev.onclick=()=>{
+
+current--;
+
+if(current<0) current=slides.length-1;
+
+showSlide(current);
+
+}
+
+dots.forEach((dot,index)=>{
+
+dot.onclick=()=>{
+
+current=index;
+
+showSlide(current);
+
+}
+
+});
+
+setInterval(()=>{
+
+current++;
+
+if(current>=slides.length) current=0;
+
+showSlide(current);
+
+},5000);
