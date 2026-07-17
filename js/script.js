fb769bcaabaf8bfb7f398e889a7ccd9c5d2d5d70
solarpedia <duttarintu6969@gmail.com>
2026-07-18 03:47:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("SolarPedia v2 Loaded Successfully!");
+console.log("SolarPedia Loaded");
+
+// Search Popup
+
+const openSearch = document.getElementById("openSearch");
+const closeSearch = document.getElementById("closeSearch");
+const searchPopup = document.getElementById("searchPopup");
+
+if (openSearch && closeSearch && searchPopup) {
+
+    openSearch.onclick = () => {
+        searchPopup.classList.add("active");
+    };
+
+    closeSearch.onclick = () => {
+        searchPopup.classList.remove("active");
+    };
+
+    window.onclick = (e) => {
+        if (e.target === searchPopup) {
+            searchPopup.classList.remove("active");
+        }
+    };
+}
