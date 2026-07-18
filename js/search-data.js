1370b170c7161eddfc39e5df40c16a8f26080d5c
solarpedia <duttarintu6969@gmail.com>
2026-07-19 00:22:05 +0530

    Create search-data.js

---
+++ js/search-data.js
@@ -0,0 +1,45 @@
+const articles = [
+
+{
+title: "PM Surya Ghar Yojana",
+url: "article.html",
+category: "Government Scheme"
+},
+
+{
+title: "How Solar Panels Work",
+url: "article.html",
+category: "Solar Guide"
+},
+
+{
+title: "Net Metering Explained",
+url: "article.html",
+category: "Guide"
+},
+
+{
+title: "Solar Inverter Guide",
+url: "article.html",
+category: "Technology"
+},
+
+{
+title: "Solar Battery Guide",
+url: "article.html",
+category: "Technology"
+},
+
+{
+title: "Rooftop Solar Installation",
+url: "article.html",
+category: "Installation"
+},
+
+{
+title: "Solar Subsidy in West Bengal",
+url: "article.html",
+category: "Government Scheme"
+}
+
+];
