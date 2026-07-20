9f1cb69ae2fb0d51270ceedf2bec0f37e096f656
solarpedia <duttarintu6969@gmail.com>
2026-07-21 01:30:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -114,3 +114,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+// =============================
+// Sticky Header
+// =============================
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
